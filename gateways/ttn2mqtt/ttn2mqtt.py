ae7689b6bc92242db83c79d4cc22744d98973a56
di3go-sona <diego.sonaglia@gmail.com>
2020-04-16 15:21:52 +0200

    republisher now works

--- gateways/ttn2mqtt/ttn2mqtt.py
+++ gateways/ttn2mqtt/ttn2mqtt.py
@@ -1,8 +1,9 @@
-#! /bin/python3
+#! /usr/bin/python3
 import time
 import ttn
 from base64 import b64decode
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 
 app_id = "foobar"
 access_key = "ttn-account-v2.some_numbers"
@@ -10,7 +11,7 @@ access_key = "ttn-account-v2.some_numbers"
 # overrides dummy secrets
 from secrets import *
 
-def make_aws_payload(msg):
+def parse_ttn_msg(msg):
 	dev_id = msg.dev_id
 	payload = msg.payload_raw
 	data = b64decode(payload).decode('ascii')
@@ -19,49 +20,53 @@ def make_aws_payload(msg):
 
 	topic = f"$aws/things/{dev_id}/shadow/update"
 
-	aws_payload = {
-		'state' : {
-			'desired' : {
-				'temperature' : t,
-				'humidity' : h,
-				'rain' : r,
-				'wind' : w,
-				'wind_direction' : wd
+	aws_payload = str({
+		"state" : {
+			"desired" : {
+				"temperature" : t,
+				"humidity" : h,
+				"rain" : r,
+				"wind" : w,
+				"wind_direction" : wd
 			}
 
 		}
-	}
-
-	print(dev_id, payload, data, measurements)
-	print(str(aws_payload))
-	return  str(aws_payload), topic
+	}).replace('\'','\"')
+	return  aws_payload, topic
 	
 	
 
-def send_mqtt_msg(payload, topic):
-	client = mqtt.Client()
-	mqtt.single(topic, payload, qos=0, retain=False, hostname="localhost",
-    port=1883, client_id="", keepalive=60, will=None, auth=None, tls=None,
+def send_aws_msg(payload, topic):
+	
+	print (f'mqtt msg sending on topic {topic}')
+	print(payload)
+
+	publish.single(topic, payload, qos=0, retain=False, hostname="localhost",
+    port=1883, client_id="ttnbridge", keepalive=60, will=None, auth=None, tls=None,
     protocol=mqtt.MQTTv311, transport="tcp")
+
 	print ('mqtt msg sent')
 
 def uplink_callback(msg, client):
     print("Received uplink from ", msg.dev_id)
-    # print(msg)
-    payload, topic = make_aws_payload(msg)
+    print(msg)
+    payload, topic = parse_ttn_msg(msg)
+    send_aws_msg(payload,topic)
 
-handler = ttn.HandlerClient(app_id, access_key)
 
-# using mqtt client
-mqtt_client = handler.data()
-mqtt_client.set_uplink_callback(uplink_callback)
-mqtt_client.connect()
+print(app_id)
+ttn_handler = ttn.HandlerClient(app_id, access_key)
+
+ttn_mqtt_client = ttn_handler.data()
+ttn_mqtt_client.set_uplink_callback(uplink_callback)
+ttn_mqtt_client.connect()
+
+
+publish.single('test', 'hola', qos=0, retain=False, hostname="localhost",
+    port=1883, client_id="ttnbridge", keepalive=60, will=None, auth=None, tls=None,
+    protocol=mqtt.MQTTv311, transport="tcp")
+
+
+
 time.sleep(60)
-mqtt_client.close()
-
-# using application manager client
-# app_client =  handler.application()
-# my_app = app_client.get()
-# print(my_app)
-# my_devices = app_client.devices()
-# print(my_devices)
+ttn_mqtt_client.close()
